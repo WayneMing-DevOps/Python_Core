480544fbeb6bf1ec8872d9ed1fb3fdd93316cb3b
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-05-14 22:29:51 +0800

    Python Day01 学习代码

---
+++ Day01/Code02.py
@@ -0,0 +1,9 @@
+"""
+    函数
+"""
+
+print("需要显示的内容")
+
+str_input = input("需要显示的内容")
+
+print(str_input)
---
+++ Day01/Code03.py
@@ -0,0 +1,12 @@
+"""
+    变量
+"""
+
+# 变量名 = Vlue
+a = "H"
+b = "c"
+c = a + b
+
+class_name = "2025"
+stu_name = "zs"
+class_name = class_name + stu_name
