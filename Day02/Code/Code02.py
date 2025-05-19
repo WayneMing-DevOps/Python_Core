a565b76824309b7a5eb8b444f0520e93da2fc22a
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-05-19 22:29:30 +0800

    Python Day02 运算符

---
+++ Day02/Code/Code02.py
@@ -0,0 +1,19 @@
+"""
+    算数运算符
+    比较运算符
+"""
+
+num01 = 5
+num02 = 2.1
+print(num01 + num02)
+
+print(num01 % 2 == 0)
+print(67 % 10)
+
+num03 = 1
+print(num03 + 1)
+print(num03)
+
+num04 = 800
+num04 = num04 + 1
+num04 += 1
