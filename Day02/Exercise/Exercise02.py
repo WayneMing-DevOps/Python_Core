0a3193faafd93bc435b2cde739ebb5393d0d4fdc
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-05-21 00:02:20 +0800

    Python Day02 练习

--- Day02/Exercise/Exercise02.py
+++ Day02/Exercise/Exercise02.py
@@ -4,13 +4,15 @@
 """
 
 # 1. 获取数据
-str_Price = input("商品单价: ")
-str_Number = input("商品数量: ")
-str_Amount = input("获取金额: ")
+str_Unit_Price = input("商品单价: ")
+float_Price = float(str_Unit_Price)
+
+int_Unit_Number = int(input("商品数量: "))
+float_Amount = float(input("获取金额: "))
 
 # 2. 逻辑处理
-float_Price, float_Number, float_Amount = float(str_Price), float(str_Number), float(str_Amount)
-float_Result = float_Amount - (float_Price * float_Number)
+# float_Price, float_Number, float_Amount = float(str_Price), float(str_Number), float(str_Amount)
+float_Result = float_Amount - (float_Price * int_Unit_Number)
 
 # 3. 输出结果
 print("应找回金额: " + str(float_Result))
