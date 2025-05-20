578eebe5c821b497cfb23f882559e23f3bbcf0c7
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-05-20 23:56:52 +0800

    Python Day02 练习

---
+++ Day02/Exercise/Exercise02.py
@@ -0,0 +1,16 @@
+"""
+    在控制台中获取一个商品单价，再获取一个商品数量，再获取一个金额。
+        计算：应该找回多少钱
+"""
+
+# 1. 获取数据
+str_Price = input("商品单价: ")
+str_Number = input("商品数量: ")
+str_Amount = input("获取金额: ")
+
+# 2. 逻辑处理
+float_Price, float_Number, float_Amount = float(str_Price), float(str_Number), float(str_Amount)
+float_Result = float_Amount - (float_Price * float_Number)
+
+# 3. 输出结果
+print("应找回金额: " + str(float_Result))
