2f2709bc50ffa72f68c1d8450f4dc9597932e1b8
WayneMing-Bit01 <badmanweimin@gmail.com>
2025-05-21 23:19:54 +0800

    Python Day02 练习

--- Day02/Exercise/Exercise07.py
+++ Day02/Exercise/Exercise07.py
@@ -3,3 +3,10 @@
     计算几小时零几分钟零几秒钟.
 """
 
+int_Second = int(input("请输入总秒数:"))
+
+int_Hour = int_Second // 3600
+int_Minute = int_Second // 60 % 60
+int_Second_Re = int_Second % 60
+
+print(str(int_Hour) + "小时" + str(int_Minute) + "分钟" + str(int_Second_Re) + "秒")
